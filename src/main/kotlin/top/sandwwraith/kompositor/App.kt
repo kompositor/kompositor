d94d9384791dd3c9f39ab3d80ff34d9e7cb34fa1
Leonid Startsev <sandwwraith@gmail.com>
2017-12-16 00:36:30 +0300

    Better error collecting

--- src/main/kotlin/top/sandwwraith/kompositor/App.kt
+++ src/main/kotlin/top/sandwwraith/kompositor/App.kt
@@ -6,6 +6,7 @@ import java.io.Reader
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 fun toFileConsumer(converter: (Reader, BufferedWriter) -> Unit): (Reader, Path) -> Unit = { reader, outPath ->
     outPath.parent.toFile().mkdirs()
@@ -13,18 +14,27 @@ fun toFileConsumer(converter: (Reader, BufferedWriter) -> Unit): (Reader, Path) 
     converter(reader, writer).also { reader.close(); writer.close(); }
 }
 
+private fun reportAndBail(where: String, err: CompositeException): Nothing {
+    println("Following errors occurred when $where:")
+    err.errors.forEach {
+        println(it.message)
+    }
+    exitProcess(-1)
+}
+
 fun main(args: Array<String>) {
     // download layers
     val l = LayerDownloader(listOf("jackson-kotlin", "junit"), ::parseYamlLayer).apply { start() }
     // pulled layers and user settings, getLayers() await all parallel downloads
-    val dependencies = mergeLayers(l.getLayers().values.toList())
+    val layers = l.getLayers().fold({ it }, { reportAndBail("pulling requested layers", it) })
+    val dependencies = mergeLayers(layers.values.toList())
     val userSettings = mapOf("kotlin.version" to ("1.2.10"))
     // Mustache converter with data
     val converter = MustacheConverter().converter(dependencies + userSettings)
     // download and execute main template to 'tmp' folder in current dir
     val d = TemplateDownloader(toFileConsumer(converter), Paths.get("tmp")).apply { start() }
     //await all async operations
-    d.await()
+    d.await().fold({ }, { reportAndBail("instantiating a template", it) })
     // shutdown threadpool so we don't need to wait idle time for threads to die
     FuelManager.instance.executor.shutdown()
     println("OK")
